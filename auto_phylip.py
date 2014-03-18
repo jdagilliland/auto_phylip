7e22633df60f53574e15c1e86a146e8e02222cee
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2014-03-18 15:17:19 -0400

    Add command to run phylip on a given input file

--- auto_phylip.py
+++ auto_phylip.py
@@ -2,6 +2,11 @@
 auto_phylip contains functions to handle Phylip programs.
 '''
 import csv
+import os
+import subprocess as sub
+
+phy_exec_default = ['phylip','dnapars']
+lst_phy_opts_default = ['S','Y','I','4','5','.']
 
 def tab2phy(tabfile, germline=None, outfile=None):
     with open(tabfile,'rb') as f:
@@ -28,4 +33,61 @@ def tab2phy(tabfile, germline=None, outfile=None):
 
 def _phyrow(name, sequence):
     return str(name).ljust(10,' ') + str(sequence) + '\n'
+
+def run_phylip(
+    phy_in,
+    phy_exec=phy_exec_default,
+    lst_phy_opts=lst_phy_opts_default,
+    ):
+    """
+    Run a phylip program with a given phylip executable, and given options.
+
+    The given phylip program is run on the given input file, with the given
+    options, and the output files 'outfile' and 'outtree' are renamed to
+    `basename`.out, and `basename`.tree respectively.
+    `basename` is derived from the input file name.
+
+    Parameters
+    ----------
+    phy_in : str
+        Filename of phylip formatted file to use as input.
+    phy_exec : list, optional
+        List of strings which are the command line args needed to call
+        the desired phylip command, default is ['phylip', 'dnapars']
+    lst_phy_opts : list, optional
+        List of strings which are the interactive input options for the
+        desired phylip program.
+        These need to be carefully considered prior to running,
+        and must be in the proper order to be meaningful.
+        Default is: ['S','Y','I','4','5','.'].
+    """
+    basename = phy_in.rpartition('.')[0]
+    # remove old files
+    if os.path.lexists('infile'):
+        os.remove('infile')
+    if os.path.lexists('outfile'):
+        os.remove('outfile')
+    if os.path.lexists('outtree'):
+        os.remove('outtree')
+    # write a command file with the specified options
+    cmdfname = '.cmdfile'
+    with open(cmdfname,'w') as f:
+        f.write(phy_in + '\n')
+        for arg in lst_phy_opts:
+            f.write(arg + '\n')
+        f.write('Y')
+    # open phylip process
+    p = sub.Popen(phy_exec, stdin=sub.PIPE)
+    # send command file contents as input, wait for output
+    p.communicate(open(cmdfname,'r').read())
+    # rename output files
+    try:
+        os.rename('outfile', basename + '.out')
+        os.rename('outtree', basename + '.tree')
+    except:
+        print('The expected output was not generated. Phylip may have failed')
+        raise
+    try:
+        os.remove(cmdfname)
+    except: raise
     return None
---
+++ run_phylip.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import sys
+
+import auto_phylip
+
+if __name__ == '__main__':
+    auto_phylip.run_phylip(sys.argv[1])
+
