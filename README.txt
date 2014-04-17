e24155888e3dea77755681d0af710b615d778db0
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2014-04-17 15:08:43 -0400

    Add files to polish up the distribution
    Including:
    CHANGES.txt
    LICENSE.txt
    MANIFEST.in
    README.txt
    setup.py

---
+++ README.txt
@@ -0,0 +1,11 @@
+# AutoPHYLIP
+
+**Jason Gilliland**
+
+These are some utility scripts for dealing with PHYLIP phylogenetic
+analysis programs.
+
+To install:
+```
+python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='AutoPHYLIP',
+        version='0.1.0',
+        author='J. D. A. Gilliland',
+        author_email='jdagilliland@gmail.com',
+        py_modules=['auto_phylip'],
+        scripts=['bin/run_phylip','bin/tab2phy'],
+        license='LICENSE',
+        description='Utilities to help with using PHYLIP',
+        long_description=open('README.txt').read(),
+        )
