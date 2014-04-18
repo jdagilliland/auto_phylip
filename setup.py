3f28430138e24eb66d31d3d94a13bc6e328cfc18
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2014-04-18 12:40:00 -0400

    Fix reference to LICENSE.txt file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         author_email='jdagilliland@gmail.com',
         py_modules=['auto_phylip'],
         scripts=['bin/run_phylip','bin/tab2phy'],
-        license='LICENSE',
+        license='LICENSE.txt',
         description='Utilities to help with using PHYLIP',
         long_description=open('README.txt').read(),
         )
