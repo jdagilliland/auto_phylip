5313e9057149618dfa0b0be1f7af8fc031498e5c
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2014-05-26 21:45:44 -0400

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='AutoPHYLIP',
-        version='0.1.0',
+        version='0.1.1',
         author='J. D. A. Gilliland',
         author_email='jdagilliland@gmail.com',
         py_modules=['auto_phylip'],
